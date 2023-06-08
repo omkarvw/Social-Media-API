291c56e6c08803c36e86379c83ccafb9a6aaed36
omkarvw <womkar0203@gmail.com>
2023-06-08 23:55:23 +0530

    Changed name of api

--- config/connect.js
+++ config/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
-        console.log('successfully connected to MongoDB')
+        console.log('Successfully connected to MongoDB')
     } catch (err) {
         console.log(err)
     }
